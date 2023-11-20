882e8006bc61f7a9d734a013d9485872db83e6a7
Saurabh <asaurabh2003@gmail.com>
2023-11-19 23:57:18 -0500

    Added unit tests for Counter component

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,45 @@
 // import necessary react testing library helpers here
+import { render, screen, act } from '@testing-library/react';
 // import the Counter component here
+import Counter from '../components/Counter';
+
+let container;
 
 beforeEach(() => {
   // Render the Counter component here
+  container = render(<Counter />).container;
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  const counterHeader = screen.getByText(/Counter/i);
+  expect(counterHeader).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  const initialCount = screen.getByTestId('count');
+  expect(initialCount).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const incrementButton = screen.getByText('+');
+  const countDisplay = screen.getByTestId('count');
+
+  act(() => {
+    incrementButton.click();
+  });
+  expect(countDisplay).toHaveTextContent('1');
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const decrementButton = screen.getByText('-');
+  const countDisplay = screen.getByTestId('count');
+
+  act(() => {
+    decrementButton.click();
+  });
+  expect(countDisplay).toHaveTextContent('-1');
 });
